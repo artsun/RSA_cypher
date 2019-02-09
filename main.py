e1ca68ab73d04023233ad90188ba073ae6083c40
artsun <parafo@gmail.com>
2019-02-09 03:52:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,20 @@
-import time
-start_time = time.time()
-from random import random, seed, randint, randrange
-import pyximport; pyximport.install()
-from ctest import *
-from helpers import *
-import sys
-sys.setrecursionlimit(1000)
-from math import gcd
+import pyximport
+pyximport.install()
+from calc import *
 
-#16777215 #‭111111111111111111111111‬ #8388608
-#‭576460752303423488‬
-#‭100000000000000000000000000000000000000000000000000000000000‬ #60 bit
-#‭1152921504606846975‬
-#‭111111111111111111111111111111111111111111111111111111111111‬
+from random import randint
+e, n, d = genkeys(randint(0, 99999), randint(0, 99999))
+print("e, n, d: ", e, n, d)
+print("sizes: ", e.bit_length(), n.bit_length(), d.bit_length())
+message = str(input())
 
-seed()
+mes = list()
+for ch in message:
+    mes.append(modexp(ord(ch), e, n))
+print(mes)
 
-p = randint(0, 99999)
-p = prime_n(p)
-q = randint(0, 99999)
-q = prime_n(q)
+word = str()
+for num in mes:
+    word += (chr(modexp(num, d, n)))
 
-n = p * q
-
-fi_n = (p-1) * (q-1)
-print("fi_n", fi_n)
-e = 65537
-
-
-print("gcd:", gcd(e, fi_n))
-d = bezout(fi_n, e)
-print("d=", d)
-enc = modexp(17, e, n)
-print("enc", enc)
-print("dec", modexp(enc, d, n))
-
-
-print("--- %s seconds ---" % (time.time() - start_time))
-
-
-if __name__ == "__main__":
-  gen_keys()
+print(word)
